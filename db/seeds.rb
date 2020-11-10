a633f655672c5a9db8864b46d222a6bf8c72ed84
Meehir Patel <meehir_patel@hotmail.co.uk>
2020-11-10 14:48:30 +0000

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+	Task.create(
+	  title: Faker::Movies::HarryPotter.character,
+	  details: Faker::Movies::HarryPotter.spell,
+	  completed: false
+		)
+	puts "complete"
+end
